f22e7105e259390e214fd49eaac1e23666757e1f
FlackAP <flack.ap@gmail.com>
2013-10-07 19:04:12 -0400

    added alertify notifications

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,8 @@
 console.log('\'Allo \'Allo!');
+$(document).ready(function(){
+	$( ".about" ).click(function() {
+  		alertify.alert( "Prepare your mind for internet wizardry! Coming soon!");
+  		alertify.log("Created by Andy Flack", "", 5000);
+	});
+
+})
