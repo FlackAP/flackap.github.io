ec1acb55f3beddce56aad8a620a428e0e981808e
FlackAP <flack.ap@gmail.com>
2013-10-08 09:23:49 -0400

    fixed position of modal

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,7 @@
 console.log('\'Allo \'Allo!');
 $(document).ready(function(){
 	$( ".about" ).click(function() {
-  		alertify.alert( "Prepare your mind for internet wizardry! Coming soon!");
+  		alertify.alert( "Prepare your heart and mind for internet wizardry! Coming soon!");
   		alertify.log("Created by Andy Flack", "", 5000);
 	});
 
