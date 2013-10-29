55e390c2ab7db0e4ae315391252be4e4d484371d
FlackAP <flack.ap@gmail.com>
2013-10-28 21:16:41 -0400

    started work on contact modal

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,19 @@
 console.log('\'Allo \'Allo!');
 $(document).ready(function(){
+
 	$( ".about" ).click(function() {
   		alertify.alert( "Prepare your heart and mind for internet wizardry! Coming soon!");
   		alertify.log("Lovingly crafted by Andy Flack", "", 5000);
 	});
 
+	$('.contact').click(function(){
+		$('.contact-box').addClass('contact-active')
+
+	})
+
 
 window.fitText( document.getElementById("responsive_headline"), .5 );
-window.fitText( document.getElementById("responsive_subtitle"), 1.5 );
+window.fitText( document.getElementById("responsive_about"), 1.5 );
+window.fitText( document.getElementById("responsive_contact"), 2.5 );
 })
+
