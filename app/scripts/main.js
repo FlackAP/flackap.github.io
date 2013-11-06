dc95c3c6953e08b8cd2b76dc9333984535b91e6d
FlackAP <flack.ap@gmail.com>
2013-11-06 18:33:51 -0500

    added bio section and stylings with link from top of page to bio

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -16,31 +16,26 @@ $(document).ready(function(){
 	})
 
 	  /* Every time the window is scrolled ... */
-    $(window).scroll( function(){
+
     
-        /* Check the location of each desired element */
-        $('.animated').each( function(i){
-            
-            var bottom_of_object = $(this).position().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-            
-            /* If the object is completely visible in the window, fade it it */
-            if( bottom_of_window > bottom_of_object ){
-                
-                $(this).animate({'opacity':'1'},500);
-                $(this).addClass('animated-active');
-                    
-            }
-            
-        }); 
-    
-    });
-    
+    $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+    });    
 
 
 
 	window.fitText( document.getElementById("responsive_headline"), 0.75 );
 	window.fitText( document.getElementById("responsive_contact"), 1.75 );
 	window.fitText( document.getElementById("responsive_portfolio2"), 1.5 );
+    window.fitText( document.getElementById("responsive_bio"), 1.5 );
 })
 
