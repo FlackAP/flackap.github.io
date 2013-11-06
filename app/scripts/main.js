4066096b94fb2177eb25c8247d8d8ec4aa824c63
FlackAP <flack.ap@gmail.com>
2013-11-06 16:58:36 -0500

    added a simple subheader

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -15,10 +15,32 @@ $(document).ready(function(){
 
 	})
 
+	  /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+    
+        /* Check the location of each desired element */
+        $('.animated').each( function(i){
+            
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},500);
+                $(this).addClass('animated-active');
+                    
+            }
+            
+        }); 
+    
+    });
+    
 
 
-window.fitText( document.getElementById("responsive_headline"), 0.75 );
-window.fitText( document.getElementById("responsive_contact"), 1.75 );
-window.fitText( document.getElementById("responsive_portfolio2"), 1.5 );
+
+	window.fitText( document.getElementById("responsive_headline"), 0.75 );
+	window.fitText( document.getElementById("responsive_contact"), 1.75 );
+	window.fitText( document.getElementById("responsive_portfolio2"), 1.5 );
 })
 
