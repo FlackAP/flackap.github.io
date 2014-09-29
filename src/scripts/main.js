fbbb59fbd23ab8f03c93408cd8936f2fb3da365a
Andy Flack <flack.ap@gmail.com>
2014-09-29 16:50:29 -0400

    a lot of stuff. content updates up in it

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -26,3 +26,79 @@ $(function() {
     }
   });
 });
+
+(function($) {
+
+  /**
+   * Copyright 2012, Digital Fusion
+   * Licensed under the MIT license.
+   * http://teamdf.com/jquery-plugins/license/
+   *
+   * @author Sam Sehnert
+   * @desc A small plugin that checks whether elements are within
+   *     the user visible viewport of a web browser.
+   *     only accounts for vertical position, not horizontal.
+   */
+
+  $.fn.visible = function(partial) {
+    
+      var $t            = $(this),
+          $w            = $(window),
+          viewTop       = $w.scrollTop(),
+          viewBottom    = viewTop + $w.height(),
+          _top          = $t.offset().top,
+          _bottom       = _top + $t.height(),
+          compareTop    = partial === true ? _bottom : _top,
+          compareBottom = partial === true ? _top : _bottom;
+    
+    return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+
+  };
+    
+})(jQuery);
+
+var win = $(window);
+
+var allMods = $(".about-photo");
+
+allMods.each(function(i, el) {
+  var el = $(el);
+  if (el.visible(true)) {
+    el.addClass("already-visible"); 
+  } 
+});
+
+win.scroll(function(event) {
+  
+  allMods.each(function(i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+      el.addClass("come-in"); 
+    } 
+  });
+  
+});
+
+// Blog Feed
+function tumblrFeed(url, $el) {
+   $.ajax({
+     url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent(url),
+     dataType: 'json',
+     success: function(data) {
+       
+       var entries = data.responseData.feed.entries;
+       var fragment = '';
+       
+       if(entries) {
+          
+          for(var i=0;i < 4; i++) {
+            if (entries[i]) {
+              fragment += '<li><a href="'+entries[i].link+'">'+entries[i].title+'</a></li>';
+            }
+          }
+         
+         $el.append(fragment);
+         }
+       }
+   });
+}
