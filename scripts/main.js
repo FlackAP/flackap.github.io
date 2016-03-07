4d9ab91e031be0316ca72980c0fb225a029aba2a
Andy Flack <flack.ap@gmail.com>
2016-03-06 21:50:30 -0500

    Deployed: added fancybox to bower.json and edited css on projects section
    generated from commit fa47f011a1d5fa6192e210502b91a8efa5a44c35

--- scripts/main.js
+++ scripts/main.js
@@ -13,7 +13,7 @@ $('#menu-open').click(function(e) {
 });
 // Scrolls to the selected menu item on the page
 $(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
